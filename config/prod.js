ac75413fe825a83d2e91e374dddecb15302f40d9
Sohil <sohilmomin711@gmail.com>
2022-12-11 11:56:54 +0530

    Revert "Fixed the wrong env variable"
    This reverts commit c3d2b344fd3e39b05fd236b649aefcc8a522b8e7.

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     MONGOURI: process.env.MONGOURI,
-    JWT_SECRET: process.env.JWT_SECRET,
+    JWT_SECRET: process.env.JWT_SEC,
     SENDGRID_API: process.env.SENDGRID_API,
     EMAIL: process.env.EMAIL
 }
