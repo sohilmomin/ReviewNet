c3d2b344fd3e39b05fd236b649aefcc8a522b8e7
Mohamadsohil <47827350+sohilmomin@users.noreply.github.com>
2022-12-11 11:49:21 +0530

    Fixed the wrong env variable

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
     MONGOURI: process.env.MONGOURI,
-    JWT_SECRET: process.env.JWT_SEC,
+    JWT_SECRET: process.env.JWT_SECRET,
     SENDGRID_API: process.env.SENDGRID_API,
     EMAIL: process.env.EMAIL
 }
